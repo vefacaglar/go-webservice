b1d3a45ac743d80caa6c435097f728265ab247d6
Vefa Çağlar <gitlanburdan@windowslive.com>
2019-09-20 21:52:19 +0300

    ....

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/vefacaglar/webservice/models"
+)
 
 func main() {
 	fmt.Println("running...")
+	user := models.User{
+		ID:        2,
+		FirstName: "Vefa",
+		LastName:  "Caglar",
+	}
+
+	fmt.Println(user)
 }
